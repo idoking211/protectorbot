ba475f744cb04882c0181a5ef2c50a1c4dd9fa02
idoking211 <38375409+idoking211@users.noreply.github.com>
2018-05-16 18:06:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ client.on('message', msg => {
 
   client.on('message', msg => {
     if (msg.content === `/network creator`) {
-      msg.channel.sendMessage('The Creator of the Bot is `Derpy#6522`');
+      msg.channel.sendMessage('The Creator of the Bot is `Marshi [DEVELOPER]#0526`');
     }
   });
 
