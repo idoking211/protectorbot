3624676709e72347e48e7693c4cce88d3b82c66e
idoking211 <38375409+idoking211@users.noreply.github.com>
2018-05-17 17:16:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -292,7 +292,7 @@ bot.on('message', msg => {
 
 bot.on('message', msg => {
   if (msg.content === '#help') {
-    msg.reply(`Check your Direct Messages!`)
+    msg.reply(`Check Your DM!`)
   }
 });
 });
